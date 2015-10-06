bcb8dde891252014f103b091a78b802a53aa293f
deepsnow <doug.axtell@gmail.com>
2015-10-06 15:25:56 -0400

    I've added a table, dbo.TimeLogs, in the embedded SQL Server instance, but I don't know how to get the code to query that table

---
+++ TimeLogging/TimeLogging/SQL/dbo.TimeLogs.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TimeLogs] (
+    [Id]        INT            NOT NULL,
+    [UserId]    NVARCHAR (128) NOT NULL,
+    [StartTime] DATETIME       NOT NULL,
+    [EndTime]   DATETIME       NOT NULL,
+    [Comment]   VARCHAR (MAX)  NOT NULL,
+    [Billable] BIT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_AspNetUsers_Id] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
